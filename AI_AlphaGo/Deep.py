7f6c35b0cf4e709a3b331a36a78c5252560a8659
NgoVietThuyet <thuyet30042005@gmail.com>
2025-04-24 13:12:42 +0700

    Update Deep.py

--- AI_AlphaGo/Deep.py
+++ AI_AlphaGo/Deep.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import numpy as np
-import tensorflow as tf
+
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Constant import RED, YELLOW, IDLE
